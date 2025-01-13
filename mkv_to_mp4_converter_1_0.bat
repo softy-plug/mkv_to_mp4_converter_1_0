9989d61fa0f24f8fff39272552f0355f1ba5bad0
Softy Plug <132814762+softy-plug@users.noreply.github.com>
2025-01-13 22:11:46 +0300

    mkv_to_mp4_converter_1_0
    mkv_to_mp4_converter_1_0 - program for converting .mkv to .mp4 video files using FFmpeg

---
+++ mkv_to_mp4_converter_1_0.bat
@@ -0,0 +1,31 @@
+::[FFmpeg mkv_to_mp4_1_0 softy_plug]
+@echo off
+
+:again
+
+mkdir out
+for %%i in (*.mkv) do (
+  ffmpeg -i "%%i"  -c:v libx264 -c:a aac "out/%%~ni.mp4"  
+)
+
+
+::[FFmpeg mkv_to_mp4_1_0 softy_plug]
+
+if NOT ["%errorlevel%"]==["0"] goto:error
+echo [92m%~n1 Done.[0m
+shift
+if "%~1" == "" goto:end
+goto:again
+:error
+
+echo [93mError! Video files to encode were not found. Check for video files in the folder with the executable .exe file.[0m
+pause
+exit 0
+:end
+
+cls
+echo [92mEncoding succesful. This window will close after 10 seconds.[0m
+timeout /t 10
+
+
+::[FFmpeg mkv_to_mp4_1_0 softy_plug]
---
+++ readme.txt
@@ -0,0 +1,10 @@
+╭━━╮╱╭━┳╮╱╱╱╭━╮╱╱╱╱╱╱
+┃━━╋━┫━┫╰┳┳╮┃╋┣╮╭┳┳━╮
+┣━━┃╋┃╭┫╭┫┃┃┃╭┫╰┫┃┃╋┃
+╰━━┻━┻╯╰━╋╮┃╰╯╰━┻━╋╮┃
+╱╱╱╱╱╱╱╱╱╰━╯╱╱╱╱╱╱╰━╯
+softy_plug
+
+# mkv_to_mp4_converter_1_0
+ mkv_to_mp4_converter_1_0 - program for converting .mkv to .mp4 video files using FFmpeg
+
